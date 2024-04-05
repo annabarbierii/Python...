541b668649ac0e474ce5938736047409202d126d
annabarbierii <160878150+annabarbierii@users.noreply.github.com>
2024-04-05 11:12:48 -0300

    Create exer3.js

---
+++ primeiro.trimestre/exer3.js
@@ -0,0 +1,6 @@
+window. alert ("Por favor digite as duas medidas em centímetros ou em metros.")
+let base=Number(prompt ("Digite a medida da base do triângulo: "))
+let altura=Number(prompt ("Digite a medida da altura do triângulo: "))
+
+let area=base*altura/2
+window.alert ("A área do triângulo é "+area)
