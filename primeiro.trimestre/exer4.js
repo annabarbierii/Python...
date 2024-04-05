e1015081ca5cfecebddc2e5f3fdba34e9cf23342
annabarbierii <160878150+annabarbierii@users.noreply.github.com>
2024-04-05 11:13:49 -0300

    Create exer4.js

---
+++ primeiro.trimestre/exer4.js
@@ -0,0 +1,7 @@
+let nota1=Number (prompt("Digite a nota do primeiro bimestre: "))
+let nota2=Number (prompt ("Digite a nota do segundo bimestre: "))
+let nota3=Number(prompt("Digit e a nota do terceiro bimestre: "))
+let nota4=Number (prompt ("Digite a nota do quarto bimestre: "))
+
+let media=(nota1+nota2+nota3+nota4) /4
+window.alert ("A média das notas é: "+media)
