3e3c8f077abf63084efdbf16661fd0723348de72
annabarbierii <160878150+annabarbierii@users.noreply.github.com>
2024-04-05 11:22:31 -0300

    Create exer7.js

---
+++ primeiro.trimestre/exer7.js
@@ -0,0 +1,13 @@
+let numerol=Number (prompt("Digite o primeiro número: "))
+let numero2=Number (prompt("Digite o segundo número: "))
+let numero3=Number (prompt("Digite o terceiro número: "))
+
+if (numerol>numero2 && numero1>numero3){
+ window.alert ( "O maior número é: "+numero1)
+}
+else if (numero2>numerol && numero2>numero3){
+ window.alert ( "O maior número é: "+numero2)
+}
+ else{
+ window.alert ( "O maior número é: "+numero3)
+ }
